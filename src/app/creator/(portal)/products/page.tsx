a944dcec39ece185597036a55c16d8597ba911a0
phillous <theophiluseniayewu@gmail.com>
2026-07-26 23:05:16 +0100

    added force dynamic

--- src/app/creator/(portal)/products/page.tsx
+++ src/app/creator/(portal)/products/page.tsx
@@ -7,6 +7,7 @@ import { CreatorProductCard } from "./CreatorProductCard";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
 
 
 function formatNaira(kobo: number) {
